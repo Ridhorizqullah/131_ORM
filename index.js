092bdb5c0d2067b27027a7ae5187c1d67d378bee
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-10-29 12:22:21 +0700

    menambahkan endpoint

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3000;
 const db = require('./models');
+const komik = require('./models/komik');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port,() => {
-    console.log(`Server is started on port 3000`);
+    console.log(`Server is started on port`);
 })
 
-app.sequelize.sync()
+db.sequelize.sync()
     .then((result) => {
         app.listen(port, () => {
             console.log(`Server is strated`);
@@ -27,5 +28,45 @@ app.post('/komik', async (req, res) => {
         res.status(komik);
     } catch (error) {
     res.send({message : error.message});
+    }
+});
+
+app.get('/komik', async (req, res) => {
+    try{
+        const komiks = await db.Komik.findAll();
+        res.send(komiks);
+    }
+    catch (error){
+        res.send({message : error.message});
+    }
+});
+
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.update(data);   
+        res.send({ message: 'Komik berhasil di update' });
+    } 
+    catch (error) {
+        res.send({ message: error.message });
+    }
+});
+
+app.delete('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.destroy();
+        res.send({ message: 'Komik berhasil dihapus' });
+    } catch (error) {
+        res.send({ message: error.message });
     }
 });
